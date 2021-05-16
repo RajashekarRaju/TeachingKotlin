63047373aa91f39cddb2fab8397480281770621f
SulthanDudekula <sulthan.cse27@gmail.com>
2021-05-16 16:49:39 +0530

    Added a new topic DataTypes

---
+++ src/main/kotlin/DataType/Example.kt
@@ -0,0 +1,27 @@
+package DataType
+
+/** DataTypes Example
+
+ */
+private fun main(){
+    val rollNumber: Int = 45
+    val nameOfTheStudent: String = "BOb"
+    val phoneNumber: Long = 9792939400
+    val grade: Char = 'A'
+    val isANiceStudent: Boolean = true
+    val height: Double = 5.8
+    val backLogs: Int =0
+    val studentName: String = """My name is $nameOfTheStudent"""
+    val studentRollno: String = """Roll Number : $rollNumber"""
+    val studentphoneNumber: String = """PhoneNumber : $phoneNumber"""
+    val studentgrade: String = """Grade : $grade"""
+    val studentheight: String = """Height: $height"""
+    val studentBacklogs: String = """Backlogs:$backLogs"""
+
+    println(studentName)
+    println(studentRollno)
+    println(studentphoneNumber)
+    println(studentgrade)
+    println(studentheight)
+    println(studentBacklogs)
+}
