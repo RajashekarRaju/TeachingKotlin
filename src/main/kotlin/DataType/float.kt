8f6f060a8508b3b61b725db1eab891f7bbb512b2
SulthanDudekula <sulthan.cse27@gmail.com>
2021-05-17 19:46:46 +0530

    Added a new topics in DataTypes first api program

---
+++ src/main/kotlin/DataType/float.kt
@@ -0,0 +1,19 @@
+package DataType
+
+/**
+ * Float Datatype
+ * Floating point data type is used to store decimal values.
+ * Kotlin provide Floating point data type Float and Double.
+ * In Kotlin,it will took Double as default data type for decimal values.
+ */
+fun main(){
+
+    //Declaring Float by add suffix F to value.
+    val floatValue:Float = 1.56F
+
+    //Declaring Double
+    val doubleValue:Double = 12.12121212
+
+    println("Print the value of floatValue : $floatValue")
+    println("Print the value of doubleValue : $doubleValue")
+}
