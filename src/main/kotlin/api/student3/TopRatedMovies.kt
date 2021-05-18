233f0dd686546ab08932b6765d87c3ed04bcc61a
sravanthi4321 <gujarathisravanthi7@gmail.com>
2021-05-18 15:50:41 +0530

    movie data

--- src/main/kotlin/api/student3/TopRatedMovies.kt
+++ src/main/kotlin/api/student3/TopRatedMovies.kt
@@ -1,4 +1,21 @@
 package api.student3
 
 class TopRatedMovies {
+   // val movie= Movie("julai")
+    val moviesList:  List<Movie> = listOf(
+       Movie("bahubali",4321),
+   Movie("ismartshenkar",4322),
+    Movie("jannu",4323)
+    )
+}
+fun main() {
+    val TopRatedMovies = TopRatedMovies()
+    val moviesList = TopRatedMovies.moviesList
+
+       moviesList.forEach {
+        val titleOfTheMovie = it.title
+        val idOfTheMovie = it.movieId
+        println("Name of the movie is $titleOfTheMovie")
+        println("Id of the movie is $idOfTheMovie")
+    }
 }
