0a86030af762220a029aa6287f11e09d841b2bfb
SulthanDudekula <sulthan.cse27@gmail.com>
2021-05-18 17:44:45 +0530

    Completed Exercise on making simple http connection and returning jsons data

--- src/main/kotlin/api/student4/UpcomingMovies.kt
+++ src/main/kotlin/api/student4/UpcomingMovies.kt
@@ -8,15 +8,10 @@ class UpcomingMovies {
 
     //Declaring a variable to access to data class which is Movie
     //There are numbers of movies to access so,using list
-    val moviesList: List<Movie> = listOf(
-        //initializing values for the parameters in data class
-        Movie("Radhe", 1234),
-        Movie("Red", 1235),
-        Movie("RRR",1236)
-    )
+    val moviesList: List<Movie> = fetchJsonData(movieResponse())
 }
 
-fun main() {
+private fun main() {
 
     //Declaring a variable to get data in the UpcomingMovies class
     val upcomingMovies = UpcomingMovies()
